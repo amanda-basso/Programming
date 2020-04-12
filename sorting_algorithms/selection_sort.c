122fa99b0b1a4290dde92e9cd67a3722b6647b0f
Amanda Oliveira <amanda.basso.oliveira@outlook.com>
2020-04-12 01:23:26 -0300

    Add implementation of selection sort in C

---
+++ sorting_algorithms/selection_sort.c
@@ -0,0 +1,32 @@
+/* This code implements the selection sort algorithm */
+#include<stdlib.h>
+#include"../utils/utils.h"
+
+void selection_sort(int *array, const int size_array){
+    int i, j, min_index;
+
+    for (i = 0; i < size_array - 1; i++){
+        min_index = i;
+        for (j = i + 1; j < size_array; j++){
+            if (*(array + j) < *(array + min_index))
+                min_index = j;
+        }
+        if (min_index != i)
+            swap((array + i), (array + min_index));
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int *array = malloc(atoi(argv[1]) * sizeof(int));
+
+    create_random_array(array, atoi(argv[1]));
+
+    print_array(array, atoi(argv[1]), "Original array");
+    selection_sort(array, atoi(argv[1]));
+    print_array(array, atoi(argv[1]), "Sorted array");
+
+    free(array);
+
+    return 0;
+}
