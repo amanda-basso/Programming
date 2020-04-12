38fe6fa89b2a51276ea6b6109c471abda3722c6f
Amanda Oliveira <amanda.basso.oliveira@outlook.com>
2020-04-11 23:21:47 -0300

    Add implementation of insertion sort in C

---
+++ sorting_algorithms/insertion_sort.c
@@ -0,0 +1,33 @@
+/* This code implements insertion sort algorithm in C */
+#include<stdlib.h>
+#include"../utils/utils.h"
+
+void insertion_sort(int *array, const int size_array){
+    int i, j, key;
+
+    for (i = 1; i < size_array; i++){
+        key = *(array + i);
+        j = i - 1;
+
+        while(j >= 0 && *(array + j) > key){
+            *(array + j + 1) = *(array + j);
+            j--;
+        }
+        *(array + j + 1) = key;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int *array = malloc(atoi(argv[1]) * sizeof(int));
+
+    create_random_array(array, atoi(argv[1]));
+
+    print_array(array, atoi(argv[1]), "Original array");
+    insertion_sort(array, atoi(argv[1]));
+    print_array(array, atoi(argv[1]), "Sorted array");
+
+    free(array);
+
+    return 0;
+}
