6586ecf44d9b09bd5ccae0ee82d39f683308e85a
Amanda Oliveira <amanda.basso.oliveira@outlook.com>
2020-04-16 14:48:57 -0300

    Add files with util functions used by sorting algorithms

---
+++ sorting_algorithms/utils.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include"utils.h"
+
+void swap(int *a, int *b){
+    int aux;
+
+    aux = *a;
+    *a = *b;
+    *b = aux;
+}
+
+void print_array(int *array, const int size_array, const char sentence[]){
+    int i;
+
+    printf("%s\n", sentence);
+    for (i = 0; i < size_array; i++)
+        printf("%d ", *(array + i));
+    printf("\n");
+}
+
+void create_random_array(int *array, const int size_array){
+    srand(time(NULL));
+    int i;
+
+    for (i = 0; i < size_array; i++)
+        *(array + i) = rand()%100;
+}
---
+++ sorting_algorithms/utils.h
@@ -0,0 +1,9 @@
+/* This file contains util functions common to the implementation of diverse sorting algorithms */
+#ifndef UTILS_H
+#define UTILS_H
+
+void swap(int *a, int *b);
+void print_array(int *array, const int size_array, const char sentence[]);
+void create_random_array(int *array, const int size_array);
+
+#endif //UTILS_H
