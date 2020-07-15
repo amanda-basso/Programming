c21104beb27cb7894afa0e57749735dc8ccfa2d8
Amanda Oliveira <amanda.basso.oliveira@outlook.com>
2020-07-14 21:05:46 -0300

    Add gets_size_string function into utils files

--- utils/utils.h
+++ utils/utils.h
@@ -5,5 +5,6 @@
 void swap(int *a, int *b);
 void print_array(int *array, const int size_array, const char sentence[]);
 void create_random_array(int *array, const int size_array);
+int gets_size_string(char *string);
 
 #endif //UTILS_H
