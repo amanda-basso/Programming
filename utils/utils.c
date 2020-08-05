f457109c3de31660188b6db0535b33e5d89dad7c
Amanda Oliveira <amanda.basso.oliveira@outlook.com>
2020-08-05 13:57:41 -0300

    Add functions to utils files

--- utils/utils.c
+++ utils/utils.c
@@ -20,12 +20,40 @@ void print_array(int *array, const int size_array, const char sentence[]){
     printf("\n");
 }
 
+void print_matrix(int **matrix, const int rows, const int columns, const char sentence[]){
+    int i, j;
+
+    printf("%s\n", sentence);
+    for (i = 0; i < rows; i++){
+        for (j = 0; j < columns; j++)
+            printf("%d  ", *(*(matrix + i) + j));
+        printf("\n");
+    }
+    printf("\n");
+}
+
 void create_random_array(int *array, const int size_array){
     srand(time(NULL));
     int i;
 
     for (i = 0; i < size_array; i++)
-        *(array + i) = rand()%100;
+        *(array + i) = rand() % 100;
+}
+
+void create_random_matrix(int **matrix, const int rows, const int columns){
+    srand(time(NULL));
+    int i, j;
+
+    for (i = 0; i < rows; i++)
+        for (j = 0; j < columns; j++)
+            *(*(matrix + i) + j) = rand() % 10;
+}
+
+void initialize_array(int *array, const int size_array, const int value){
+    int i;
+
+    for (i = 0; i < size_array; i++)
+        *(array + i) = value;
 }
 
 int gets_size_string(char *string){
@@ -35,4 +63,27 @@ int gets_size_string(char *string){
         size++;
 
     return size;
+}
+
+int partition(int *array, int low, int high){
+    int j;
+    int pivot = *(array + high);
+    int i = low - 1;
+
+    for (j = low; j < high; j++){
+        if (*(array + j) < pivot){
+            i++;
+            swap((array + j), (array + i));
+        }
+    }
+    swap((array + i + 1), (array + high));
+    return (i + 1);
+}
+
+void quick_sort(int *array, int low, int high){
+    if (low < high){
+        int partition_index = partition(array, low, high);
+        quick_sort(array, low, partition_index - 1);
+        quick_sort(array, partition_index + 1, high);
+    }
 }
